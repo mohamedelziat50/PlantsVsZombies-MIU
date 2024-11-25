139a36f9703236eca80045f1dc05e2c903cad6c2
mohamedelziat50 <mohamedelziat50@gmail.com>
2024-11-25 21:33:44 +0200

    Finished MainMenu Class; Added Level and Yard Classes; Added FileOperations Interface; and added resources directory to store binary files

--- src/MainMenu.java
+++ src/MainMenu.java
@@ -1,25 +1,336 @@
-public class MainMenu
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class MainMenu implements FileOperations
 {
-    private Player p;
+    /*
+        MEMBER VARIABLES
+     */
+    // HashMap to swiftly access all the players' data.
+    private HashMap<String, Player> PlayersGameData;
 
-    public MainMenu(Player p) {
-        this.p=p;
+    // Current player accessing the main menu.
+    private Player currentPlayer;
+
+    /*
+        CONSTRUCTORS
+     */
+    public MainMenu()
+    {
+        // Set the currentPlayer to be unknown.
+        currentPlayer = null;
+
+        // Load all players from the file, using implemented loadPlayers() function.
+        try
+        {
+            // Load players from file, so that the players' data is available for operations.
+            PlayersGameData = loadPlayers();
+        }
+        catch (Exception exception)
+        {
+            System.out.println("Exception inside MainMenu(): Error loading player data from file: " + exception);
+        }
     }
 
-    public MainMenu() {
-    }
-    //check if the username already exists else write in the file
-    public void createAccount(String username,String password,String name){
+    /*
+        METHODS
+     */
+    // Function to display stars on terminal.
+    public void displayStars()
+    {
+        // Number of stars to display.
+        final int numberOfStars = 20;
 
+        // Display them.
+        for (int i = 0; i < numberOfStars; i++)
+            System.out.print("*");
+        System.out.println();
     }
 
-    //check if the username exists in the file and if exist check if matches the password
-    public boolean login(String username,String password){
-        return false;
-    }
-    //check if the username exists in the file and if exist and matches with the pass remove it from the file and return true
-    public boolean delete(String username,String password){
-        return false;
+    // Function to display MainMenu, takes in Scanner as input for re-usability.
+    public void displayMenu(Scanner input)
+    {
+        // // Initialize user choice for the loop.
+        int userChoice = 0;
+
+        // Exit loop when the user selects 5 or successful login happens
+        while (currentPlayer == null)
+        {
+            // Display Stars (Top Border)
+            displayStars();
+
+            // Display menu options
+            System.out.println("1. Sign in");
+            System.out.println("2. Create Account");
+            System.out.println("3. Display Users");
+            System.out.println("4. Exit");
+
+            // Display Stars (Bottom Border)
+            displayStars();
+
+            // Try-Catch block in case of InputMismatch
+            try
+            {
+                // Prompt user for input
+                System.out.print("Enter your choice: ");
+                userChoice = input.nextInt();
+                input.nextLine(); // Clear buffer
+            }
+            catch (InputMismatchException exception)
+            {
+                System.out.println("Exception inside displayMenu(): User entered invalid type for input. Exiting program");
+                System.exit(1);
+            }
+
+            // Handle user choice
+            switch (userChoice)
+            {
+                // Sign in
+                case 1:
+                    System.out.println("Sign in selected.");
+                    if(!signIn(input))
+                        System.out.println("Sign-in failed.");
+                    break;
+                // Create Account
+                case 2:
+                    System.out.println("Create Account selected.");
+                    // If successful!
+                    if(createAccount(input))
+                    {
+                        // Load all players from the file, since a new account has been added.
+                        try
+                        {
+                            PlayersGameData = loadPlayers();
+                            System.out.println("Account created successfully.");
+                        }
+                        catch (Exception exception)
+                        {
+                            System.out.println("Exception inside displayMenu(): " + exception);
+                        }
+                    }
+                    else
+                        System.out.println("Account creation failed.");
+                    break;
+                // Display Users
+                case 3:
+                    System.out.println("Display Users selected.");
+                    displayUsers();
+                    break;
+                // Exit Program
+                case 4:
+                    System.out.println("Exiting... Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice! Please select a valid option.");
+                    break;
+            }
+        }
+
+        // re-set userChoice
+        userChoice = 0;
+
+        // If successful login happens
+        while(currentPlayer != null && userChoice != 1)
+        {
+            // Display Stars (Top Border)
+            displayStars();
+
+            // Display menu options
+            System.out.println("1. Start Adventure!");
+            System.out.println("2. Delete Account");
+            System.out.println("3. Exit");
+
+            // Display Stars (Bottom Border)
+            displayStars();
+
+            // Try-Catch block in case of InputMismatch
+            try
+            {
+                // Prompt user for input
+                System.out.print("Enter your choice: ");
+                userChoice = input.nextInt();
+                input.nextLine(); // Clear buffer
+            }
+            catch (InputMismatchException exception)
+            {
+                System.out.println("Exception inside displayMenu(): User entered invalid type for input. Exiting program");
+                System.exit(1);
+            }
+
+            // Handle user choice
+            switch (userChoice)
+            {
+                // Start Adventure!
+                case 1:
+                    System.out.println("Let's Start the Adventure!");
+                    break;
+                // Delete Account
+                case 2:
+                    System.out.println("Delete Account selected.");
+                    if(deleteAccount(input))
+                    {
+                        // Load all players from the file, since an account has been deleted.
+                        try
+                        {
+                            PlayersGameData = loadPlayers();
+                            System.out.println("Account deleted successfully.");
+                        }
+                        catch (Exception exception)
+                        {
+                            System.out.println("Exception inside displayMenu(): " + exception);
+                        }
+                    }
+                    else
+                        System.out.println("Account deletion failed.");
+                    break;
+                // Exit Program
+                case 3:
+                    System.out.println("Exiting... Goodbye!");
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice! Please select a valid option.");
+                    break;
+            }
+        }
+
+        // To be continued
     }
 
+    public boolean signIn(Scanner input)
+    {
+        // If there are no players, return false immediately.
+        if(PlayersGameData.isEmpty())
+        {
+            System.out.println("No players exist in the system.");
+            return false;
+        }
+
+        // Input username (in variable to be used again)
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+
+        // If player doesn't exist, return false
+        if(!PlayersGameData.containsKey(username))
+        {
+            System.out.println("Player not found.");
+            return false;
+        }
+
+        // Input password (in the complex line lol)
+        System.out.print("Enter password: ");
+
+        // If password doesn't match, return false
+        if(! PlayersGameData.get(username).getPassword().equals(input.nextLine()))
+        {
+            System.out.println("Incorrect password.");
+            return false;
+        }
+
+        // Otherwise, successful login and that the current player
+        currentPlayer = PlayersGameData.get(username);
+        return true;
+    }
+
+    public boolean createAccount(Scanner input)
+    {
+        // Input username
+        System.out.print("Enter username: ");
+        String username = input.nextLine();
+
+        // Check if user exists first!
+        if(PlayersGameData.containsKey(username))
+        {
+            System.out.println("Player already exists.");
+            return false;
+        }
+
+        // Input password
+        System.out.print("Enter password: ");
+        String password = input.nextLine();
+
+        // Create new player object
+        Player newPlayer = new Player(username, password);
+
+        // Add player object to the hashMap containing all data about players.
+        PlayersGameData.put(newPlayer.getUsername(), newPlayer);
+
+        // Try-Catch Block
+        try
+        {
+            // Write new player hashMap with the added player to the file.
+            writePlayers(PlayersGameData);
+        }
+        catch (Exception exception)
+        {
+            System.out.println("Exception inside createAccount(): Error saving account: " + exception);
+            return false;
+        }
+
+        // Display successful operation.
+        return true;
+    }
+
+    public void displayUsers()
+    {
+        if(PlayersGameData.isEmpty())
+        {
+            System.out.println("No Player Data to display.");
+            return;
+        }
+
+        // Save data into arrayList and sort it!
+        ArrayList<String> sortedUsers = new ArrayList<>(PlayersGameData.keySet());
+        Collections.sort(sortedUsers);
+
+        // Display Users
+        displayStars();
+        System.out.println("Available Users: ");
+        for(String username: sortedUsers)
+            System.out.println(username);
+        displayStars();
+    }
+
+    public void signOut()
+    {
+        if(currentPlayer != null)
+            currentPlayer = null;
+    }
+
+    public boolean deleteAccount(Scanner input)
+    {
+        // For-some reason, just check!
+        if(currentPlayer == null)
+            return false;
+
+        // Input password
+        System.out.print("Re-write password for deletion: ");
+
+        // If password doesn't match, return false and sign out player.
+        if(!currentPlayer.getPassword().equals(input.nextLine()))
+        {
+            System.out.println("Incorrect password. Signing out.");
+            signOut();
+            return false;
+        }
+
+        // Remove player from game data
+        PlayersGameData.remove(currentPlayer.getUsername());
+
+        // Try-Catch Block
+        try
+        {
+            // Write new player hashMap with the deleted player to the file.
+            writePlayers(PlayersGameData);
+        }
+        catch (Exception exception)
+        {
+            System.out.println("Exception inside deleteAccount(): Error deleting account: " + exception);
+            return false;
+        }
+
+        return true;
+    }
 }
--- src/Player.java
+++ src/Player.java
@@ -1,38 +1,61 @@
-public class Player
-{
-    private String username;
-    private String password;
-    private String name;
+import java.util.ArrayList;
+import java.io.Serializable;
 
-    public Player(String username, String password, String name) {
+public class Player implements Serializable
+{
+    /*
+        MEMBER VARIABLES
+     */
+    // Username and Password
+    private String username, password;
+
+    // Levels, indicating the levels unlocked by the player.
+    private ArrayList<Level> levelsUnlocked; // Will need a function to load levels unlocked from file.
+
+    /*
+        CONSTRUCTORS
+     */
+    public Player(String username, String password)
+    {
         this.username = username;
         this.password = password;
-        this.name = name;
     }
 
+    /*
+        GETTERS & SETTERS
+     */
     public String getUsername() {
         return username;
     }
-
     public void setUsername(String username) {
         this.username = username;
     }
-
     public String getPassword() {
         return password;
     }
-
     public void setPassword(String password) {
         this.password = password;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
+    /*
+        METHODS
+     */
 
 }
+
+/*
+    Notes for UML:
+
+    Having all plant's health and waiting time will be redundant stored inside the class, utilize yard or level.
+    The Yard class or Level can manage plant placements and interactions.
+    The Player class doesn't need to store this data directly.
+    For example, the Yard class could hold a List<Plant> representing plants currently placed in the yard.
+
+    Our way: Aggregation: If the MainMenu "owns" the Player object but the Player can exist independently
+     (e.g., multiple menus accessing the same user object), aggregation makes sense.
+
+    Composition: If the Player object is tightly bound to the MainMenu and cannot exist without it
+    (e.g., created when MainMenu is initialized and destroyed alongside it), use composition.
+
+    The Player object doesn’t directly deal with gameplay, it only tracks progress through the List<Level>.
+ */
